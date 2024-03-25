0b93b61f99a263fedfc2d474c2fd86003a9db7f0
shella <shellasitanaya@gmail.com>
2024-03-25 22:58:56 +0700

    update css

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,23 +1,104 @@
 @extends('layout.mainlayout')
 
-
 @section('head')
+<link rel="stylesheet" href ="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <style>
         body{
             color: white;
             min-height: 100vh;
+            background: linear-gradient(180deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 49%, rgba(0, 212, 255, 1) 100%);
+            background-size: cover;
+            background-position: center;
+            font-weight: 800;
         }
 
         .form-text{
             color: white
         }
+
+        .title {
+            font-size: 45px;
+            font-weight: bold;
+            margin: 20px;
+        }
+
+        .form-content {
+            border: 5px solid white;
+            padding: 30px;
+            box-shadow: 1px 5px 5px 1px #888888;
+        }
+
+         /*logo */
+         i {
+            font-size: 20px; 
+            margin-right: 5px;
+        }
+
+        .btn{  
+            background-color:#004792;
+            width: 100%;
+        }
+
+        .btn:hover{
+            background-color: #0070BB;
+        }
+
+        /* field input */
+        input[type="text"], 
+        input[type="email"], 
+        input[type="password"], 
+        select#jenisKonsumsi,
+        select, 
+        textarea {
+            background-color: transparent; 
+            border: 3px solid #ccc; 
+            border-radius: 5px;    
+            padding: 10px;
+            color: white;
+        }
+
+        input[type="file"] {
+            background-color: transparent; 
+            border: 3px solid #ccc; 
+            border-radius: 5px;    
+            color: white; 
+        }
+
+        select#jenisKonsumsi {
+            color: #72A0C1; 
+            border-color: 1px solid #004170;
+            font-size: 15px; 
+        }
+
+        /* placeholder */
+        input[type="text"]::placeholder,
+        input[type="email"]::placeholder,
+        input[type="password"]::placeholder,
+        select::placeholder,
+        select#jenisKonsumsi::placeholder,
+        textarea::placeholder {
+            color: #B0E0E6; 
+            font-size: 15px; 
+        }
+
+        /* input field */
+        input[type="text"]:focus,
+        input[type="email"]:focus,
+        input[type="password"]:focus,
+        select:focus,
+        textarea:focus {
+            background-color: rgba(255, 255, 255, 0.5);
+            border-color: 2px solid #004170 ;
+        }
+
+       
     </style>
 @endsection
 
 @section('content')
 <div class="container container-registration">
-
-    <div class="title d-flex justify-content-center">Registration</div>
+    
+    <div class="title d-flex justify-content-center">REGISTRATION</div>
 
     <div class="form-content">
 
@@ -25,8 +106,9 @@
             @csrf
             <div class="row">
                 <div class="mb-3 col-md-6">
+                    <i class="fas fa-school"></i>
                     <label for="asalSekolah" class="form-label">Asal Sekolah</label>
-                    <input type="text" class="form-control @error('asalSekolah') is-invalid @enderror" id="asalSekolah" name="asalSekolah" placeholder="" value="{{ old('asalSekolah') }}" required>
+                    <input type="text" class="form-control @error('asalSekolah') is-invalid @enderror" id="asalSekolah" name="asalSekolah" placeholder="Masukkan asal sekolah" value="{{ old('asalSekolah') }}" required>
                     @error('asalSekolah')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -37,8 +119,9 @@
 
             <div class="row">
                 <div class="mb-3 col-md-6">
+                    <i class="fas fa-user"></i>
                     <label for="usernameKelompok" class="form-label">Username Kelompok</label>
-                    <input type="text" class="form-control @error('usernameKelompok') is-invalid @enderror" id="usernameKelompok" name="usernameKelompok" placeholder="" value="{{ old('usernameKelompok') }}" required>
+                    <input type="text" class="form-control @error('usernameKelompok') is-invalid @enderror" id="usernameKelompok" name="usernameKelompok" placeholder="Masukkan username kelompok" value="{{ old('usernameKelompok') }}" required>
                     @error('usernameKelompok')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -47,8 +130,9 @@
 
             <div class="row">
                 <div class="mb-3 col-md-6">
+                    <i class="fas fa-lock"></i>
                     <label for="inputPassword5" class="form-label">Password</label>
-                    <input type="password" id="inputPassword5" class="form-control @error('passPeserta') is-invalid @enderror" name="passPeserta" aria-describedby="passwordHelpBlock" value="{{ old('passPeserta') }}" required>
+                    <input type="password" id="inputPassword5" class="form-control @error('passPeserta') is-invalid @enderror" name="passPeserta" aria-describedby="passwordHelpBlock" placeholder="Masukkan password" value="{{ old('passPeserta') }}" required>
                     @error('passPeserta')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -60,8 +144,9 @@
 
             <div class="row">
                 <div class="mb-3 col-md-6">
+                <i class="fas fa-lock"></i>
                     <label for="inputConfirmPassword5" class="form-label">Confirm Password</label>
-                    <input type="password" id="inputConfirmPassword5" class="form-control" name="passConfirmPeserta" aria-describedby="passwordHelpBlock" value="{{ old('passConfirmPeserta') }}" required>
+                    <input type="password" id="inputConfirmPassword5" class="form-control" name="passConfirmPeserta" aria-describedby="passwordHelpBlock" placeholder="Masukkan confirm password" value="{{ old('passConfirmPeserta') }}" required>
                     <div id="confirmPasswordError" class="invalid-feedback" style="display: none;">Password is not the same</div>
                     <div id="passwordHelpBlock" class="form-text">
                         Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
@@ -86,14 +171,16 @@
 
             <div class="row">
                 <div class="mb-3 col-md-6">
+                <i class="fas fa-person"></i>
                     <label for="namaKetua" class="form-label">Nama Ketua (Member 1)</label>
-                    <input type="text" class="form-control @error('namaKetua') is-invalid @enderror" id="namaKetua" name="namaKetua" placeholder="" value="{{ old('namaKetua') }}" required>
+                    <input type="text" class="form-control @error('namaKetua') is-invalid @enderror" id="namaKetua" name="namaKetua" placeholder="Masukkan nama ketua" value="{{ old('namaKetua') }}" required>
                     @error('namaKetua')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
 
                 <div class="mb-3 col-md-6">
+                <!-- <i class="fas fa-envelope"> -->
                     <label for="emailKetua" class="form-label">Email Ketua</label>
                     <input type="email" class="form-control @error('emailKetua') is-invalid @enderror" id="emailKetua" name="emailKetua" placeholder="example@gmail.com" value="{{ old('emailKetua') }}" required>
                     @error('emailKetua')
@@ -104,16 +191,18 @@
 
             <div class="row">
                 <div class="mb-3 col-md-6">
+                <!-- <i class="fas fa-person"> -->
                     <label for="namaKedua" class="form-label">Nama Member 2</label>
-                    <input type="text" id="namaKedua" name="namaKedua" class="form-control @error('namaKedua') is-invalid @enderror" placeholder="" value="{{ old('namaKedua') }}" required>
+                    <input type="text" id="namaKedua" name="namaKedua" class="form-control @error('namaKedua') is-invalid @enderror" placeholder="Masukkan nama member 2" value="{{ old('namaKedua') }}" required>
                     @error('namaKedua')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
 
                 <div class="mb-3 col-md-6">
+                <!-- <i class="fas fa-person"> -->
                     <label for="namaKetiga" class="form-label">Nama Member 3</label>
-                    <input type="text" id="namaKetiga" name="namaKetiga" class="form-control @error('namaKetiga') is-invalid @enderror" placeholder="" value="{{ old('namaKetiga') }}" required>
+                    <input type="text" id="namaKetiga" name="namaKetiga" class="form-control @error('namaKetiga') is-invalid @enderror" placeholder="Masukkan nama member 3" value="{{ old('namaKetiga') }}" required>
                     @error('namaKetiga')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -122,6 +211,7 @@
 
             <div class="row">
                 <div class="mb-3 col-md-6">
+                <!-- <i class="fas fa-utensils"> -->
                     <label for="jenisKonsumsi" class="mb-2">Jenis Konsumsi</label>
                     <select class="form-select @error('jenisKonsumsi') is-invalid @enderror" id="jenisKonsumsi" name="jenisKonsumsi" aria-label="Default select example" required>
                         <option selected>Pilih jenis konsumsi...</option>
@@ -135,6 +225,7 @@
                 </div>
 
                 <div class="mb-3 col-md-6">
+                <!-- <i class="fas fa-allergies"></i> -->
                     <label for="alergi" class="form-label">Apakah ada anggota yang mempunyai alergi?</label>
                     <input type="text" class="form-control @error('alergi') is-invalid @enderror" id="alergi" name="alergi" placeholder="Jika tidak bisa inputkan '-'" value="{{ old('jenisAlergi') }}" required>
                     @error('alergi')
@@ -153,7 +244,7 @@
                 </div>
             </div>
 
-            <button type="submit" class="btn btn-primary btn-lg mt-2">Submit</button>
+            <button type="submit" class="btn btn-primary btn-lg mt-2">SUBMIT</button>
         </form>
     </div>
 </div>
