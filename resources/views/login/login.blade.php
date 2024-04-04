21aecfaedd74163d1e79d221e9126882c54f5655
Jovan Andhika <c14220013@john.petra.ac.id>
2024-04-04 10:28:15 +0700

    temporary: login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('storage/assetsEric/login.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assetsEric/login.css') }}">
     <!-- Bootstrap icon -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
@@ -34,7 +34,7 @@
 
             <div class="row center">
                 <div class="d-flex col-lg-8">
-                    <img src="{{ asset('storage/assetsEric/spaceHerta1.png') }}" class="img-fluid " alt="">
+                    <img src="{{ asset('asset/logo-main.png') }}" class="img-fluid " alt="">
                 </div>
 
                 <div class="col-lg-4">
