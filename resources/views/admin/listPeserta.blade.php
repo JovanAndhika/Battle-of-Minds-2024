d3fcd77c16989ddca951c107ad0608b39dd900ad
William0389 <c14220049@john.petra.ac.id>
2024-05-02 18:29:54 +0700

    feat: poin

--- resources/views/admin/listPeserta.blade.php
+++ resources/views/admin/listPeserta.blade.php
@@ -61,7 +61,7 @@
                         <th>Asal sekolah</th>
                         <th>Nama kelompok</th>
                         <th>Bukti transaksi</th>
-                        <th>Email perwakilan</th>
+                        <th>Poin</th>
                         <th>Data Anggota</th>
                         <th>Validasi</th>
                     </tr>
@@ -102,7 +102,7 @@
                         </div>
                         {{-- ======================================= END MODAL ========================================= --}}
 
-                        <td>{{ $peserta->emailPerwakilan }}</td>
+                        <td>{{ $peserta->poin }}</td>
 
 
                         <td><button data-modal-target='modal-peserta{{ $loop->iteration }}' data-modal-toggle='modal-peserta{{ $loop->iteration }}' type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View
@@ -207,7 +207,7 @@
         },
         columnDefs: [{
             targets: '_all',
-            className: 'dt-body-left'
+            className: 'dt-head-left dt-body-left'
         }],
         // columns: [
         // null, null, null, {
