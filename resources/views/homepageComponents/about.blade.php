3363dc38ae33ccf9c3fadcb0a64267bd9ef9d534
Jeffry Hermawan <jeffryhermawantm@gmail.com>
2024-05-18 10:34:32 +0700

    justify text about

--- resources/views/homepageComponents/about.blade.php
+++ resources/views/homepageComponents/about.blade.php
@@ -85,7 +85,7 @@
         <img src="{{ asset('asset/logo-main.png') }}" alt="logo"
             class="animate__animated animate__bounce logo lg:w-[630px] sm:w-[500px] max-sm:w-[320px] ">
         <p
-            class="description lg:w-[760px] lg:text-lg xl:w-[900px] text-center mt-7 sm:w-[550px] sm:text-md max-sm:w-[300px] max-sm:text-sm max-sm:leading-6">
+            class="description lg:w-[760px] lg:text-lg xl:w-[900px] text-justify mt-7 drop-shadow-2xl sm:w-[550px] sm:text-md max-sm:w-[300px] max-sm:text-xs max-sm:leading-6">
             Battle of Minds 2024 merupakan sebuah lomba dengan bentuk kegiatan Numerical Logical Competition yang
             memadukan
             konsep logika matematika dengan permainan yang seru dan menantang untuk mengasah pola pikir siswa siswi
