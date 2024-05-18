36dbfae5f171d222647b1a5277520f6c460249c0
Jeffry Hermawan <jeffryhermawantm@gmail.com>
2024-05-18 10:44:12 +0700

    guide description

--- resources/views/homepageComponents/guide.blade.php
+++ resources/views/homepageComponents/guide.blade.php
@@ -16,7 +16,10 @@
         to {
             filter: drop-shadow(0px 0px 1px rgb(120, 142, 255));
         }
+    }
 
+    .guide-desc{
+        background: linear-gradient(125deg, rgba(61, 37, 84, 0.75) 0%, rgba(123, 48, 176, 0.75) 51%, rgba(120, 27, 55, 0.75) 100%);
     }
 </style>
 <section class="guide-section my-24" id="guide">
@@ -26,7 +29,7 @@
             GUIDEBOOK</h1>
     </div>
     <div class="flex flex-col justify-center items-center">
-        <h1 class="bg-white text-center rounded-xl text-black text-xl py-3 px-5 max-md:text-base">Click Bomby to open the guidebook!</h1>
+        <h1 class="guide-desc text-white text-center rounded-xl max-sm:w-9/12 md:w-1/2 text-black text-xl py-3 px-5 max-md:text-base">Click Bomby to open the guidebook!</h1>
         <a href='https://bom.petra.ac.id/pdf/Guidebook%20BOM.pdf' target="_blank" class="inline-block">
             <img src="asset/maskot-guidebook.png" alt="Guidebook Mascot" class="maskot-guide h-[500px] max-sm:h-[300px]">
         </a>
