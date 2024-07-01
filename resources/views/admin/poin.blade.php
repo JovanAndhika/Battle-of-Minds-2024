36365a2d3987a2096b2983c94e7012acd24055cf
William0389 <c14220049@john.petra.ac.id>
2024-07-01 16:30:46 +0700

    fix (admin) : ketinggalan responsive input poin

--- resources/views/admin/poin.blade.php
+++ resources/views/admin/poin.blade.php
@@ -45,7 +45,7 @@
                                             @enderror
                                             <input type="text" id="default-search" value="{{ $peserta->poin }}"
                                                 name="poin"
-                                                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                                                class="block w-full md:ps-10 md:p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
                                                 required />
                                             @error('poin')
                                                 <script>
@@ -57,7 +57,7 @@
                                                 </script>
                                             @enderror
                                             <button type="submit"
-                                                class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Update</button>
+                                                class="text-white md:absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Update</button>
                                         </div>
                                     </form>
                                 </td>
