a542061becb5cc9c3b306f13f715b21cfbfe0530
Jovan Andhika <c14220013@john.petra.ac.id>
2025-05-29 11:16:42 +0700

    fix: image in vercel.json

--- resources/views/layout/twlayout.blade.php
+++ resources/views/layout/twlayout.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-request">
     <link rel="icon" href="{{ asset('asset/icon-logo-bom.png') }}">
     <title>{{ $title }}</title>
 
