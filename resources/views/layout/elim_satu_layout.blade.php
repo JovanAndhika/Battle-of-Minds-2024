5774525e6bc5bd31b127b2fe6307dace352a1f26
Jovan Andhika <c14220013@john.petra.ac.id>
2025-05-29 12:26:25 +0700

    delete: timer elim satu

--- resources/views/layout/elim_satu_layout.blade.php
+++ resources/views/layout/elim_satu_layout.blade.php
@@ -167,60 +167,6 @@
 </body>
 
 <script>
-    var now = new Date().getTime();
-    var timer = new Date("July 22, 2024 12:47:00").getTime();
-
-    var countdownTime = timer - now;; // misalnya, 60 detik
-
-    window.onload = function() {
-        startCountdown();
-    };
-
-    function startCountdown() {
-        var countdownInterval = setInterval(function() {
-            countdownTime--;
-
-            if (countdownTime <= 0) {
-                for (var i = 1; i <= 300; i++) {
-                    $('#biggamesanswer' + i).prop('disabled', true);
-                }
-                clearInterval(countdownInterval);
-
-                Swal.fire({
-                    icon: 'info',
-                    title: 'Waktu telah habis !',
-                    showConfirmButton: false,
-                    timer: 2000
-                });
-
-                setTimeout(() => {
-                    window.location = '/view'
-                }, 2000);
-
-
-            } else if (countdownTime <= 3600000) {
-                $('#btn-soalbom').prop('hidden', false);
-            }
-
-            displayTime();
-        }, 1000);
-    }
-
-
-    function displayTime() {
-        var hours = Math.floor(countdownTime / (1000 * 60 * 60));
-        var minutes = Math.floor((countdownTime % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((countdownTime % (1000 * 60)) / 1000);
-
-        // Perbarui countdownTime setelah menghitung kembali jam, menit, dan detik
-        countdownTime = hours * (1000 * 60 * 60) + minutes * (1000 * 60) + seconds * 1000;
-
-        var timerDisplay = document.getElementById("timer");
-        timerDisplay.innerHTML = (hours < 10 ? "0" : "") + hours + ":" +
-            (minutes < 10 ? "0" : "") + minutes + ":" +
-            (seconds < 10 ? "0" : "") + seconds;
-    }
-
     function submit_jawaban() {
         document.getElementById('simpan-jawaban').submit();
     }
